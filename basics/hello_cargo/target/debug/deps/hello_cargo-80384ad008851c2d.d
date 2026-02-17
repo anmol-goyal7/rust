e7c04b484327a375f8ced1d36572d86f6e279dd1
anmol <anmolafk7@gmail.com>
2026-02-18 02:04:36 +0530

    basics

---
+++ basics/hello_cargo/target/debug/deps/hello_cargo-80384ad008851c2d.d
@@ -0,0 +1,5 @@
+/home/anmol/Desktop/rust/basics/hello_cargo/target/debug/deps/hello_cargo-80384ad008851c2d.d: src/main.rs
+
+/home/anmol/Desktop/rust/basics/hello_cargo/target/debug/deps/hello_cargo-80384ad008851c2d: src/main.rs
+
+src/main.rs:
---
+++ basics/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/anmol/Desktop/rust/basics/hello_cargo/target/debug/hello_cargo: /home/anmol/Desktop/rust/basics/hello_cargo/src/main.rs
